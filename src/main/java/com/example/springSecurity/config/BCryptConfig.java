a68d91c4fee12fb98064335471af938bb06f2b8a
YONG <yonghyunk09@gmail.com>
2024-03-25 12:03:24 +0900

    day 1

---
+++ src/main/java/com/example/springSecurity/config/BCryptConfig.java
@@ -0,0 +1,8 @@
+package com.example.springSecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BCryptConfig {
+
+}
--- src/main/java/com/example/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springSecurity/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.servlet.DispatcherType;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,6 +16,21 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(auth -> auth.disable())			// 괄호 내 람다함수
 			.headers(x -> x.frameOptions(y -> y.disable()))		// CK Editor image upload
+			.authorizeHttpRequests(auth -> auth
+					.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+					.requestMatchers("/user/login", "/user/register", 
+							"/img/**", "/css/**", "/js/**", "/error/**").permitAll()
+					.requestMatchers("/admin/**").hasAuthority("ADMIN")
+					.anyRequest().authenticated()
+					)
+					.formLogin(auth -> auth
+							.loginPage("/user/login")			// login form
+							.loginProcessingUrl("user/login") 	// UserDetailService 구현객체에서 처리해야함
+							.usernameParameter("uid")
+							.passwordParameter("pwd")
+							.defaultSuccessUrl("/user/loginSuccess", true)
+							.permitAll()
+					)
 		;
 		
 		return http.build();
